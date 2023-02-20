3b74a04a62e42b688e66694c67ef31e124533cad
dortheclosed <120745103+dortheclosed@users.noreply.github.com>
2023-02-20 11:49:35 +0400

    Update mint.py

--- mint.py
+++ mint.py
@@ -3,14 +3,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 import undetected_chromedriver as uc
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
 
+@st.experimental_singleton
+def get_driver():
+    return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 def run_session():
-    chrome_options = uc.ChromeOptions()
-    chrome_options.headless = True 
-    chrome_options.add_argument('--disable-gpu')
+    options = Options()
+    options.add_argument('--disable-gpu')
+    options.add_argument('--headless')
+
+    driver = get_driver()
     
     
-    driver = uc.Chrome(options=chrome_options)
+    #driver = uc.Chrome(options=chrome_options)
     try:
         driver.get("https://mintme.onrender.com")
         print(f" connected to page")
