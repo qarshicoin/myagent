9eff8d03090b35c4a481c07c8b0c5d35a2639780
dortheclosed <120745103+dortheclosed@users.noreply.github.com>
2023-03-25 13:17:55 +0300

    Update mint.py

--- mint.py
+++ mint.py
@@ -8,8 +8,10 @@ def run_session():
     chrome_options = uc.ChromeOptions()
     chrome_options.headless = True 
     chrome_options.add_argument("--no-sandbox")
-    prefs = {"profile.managed_default_content_settings.images": 2}
-    chrome_options.add_experimental_option("prefs", prefs)
+    chrome_options.add_argument('--no-first-run --no-service-autorun --password-store=basic')
+    chrome_options.add_argument(f'--disable-gpu')
+    
+    chrome_options.add_argument(f'--disable-dev-shm-usage')
     driver = uc.Chrome(options=chrome_options)
     try:
         driver.get("https://mrscan.onrender.com")
